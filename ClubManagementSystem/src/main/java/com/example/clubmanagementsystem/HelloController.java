099dcd982cd9b383db0927bc0575bb47fae66a93
Deelaka Dias <deelakadias2002@gmail.com>
2023-10-12 12:49:20 +0530

    Updated version
    commented done

--- ClubManagementSystem/src/main/java/com/example/clubmanagementsystem/HelloController.java
+++ ClubManagementSystem/src/main/java/com/example/clubmanagementsystem/HelloController.java
@@ -14,20 +14,24 @@ public class HelloController {
         System.out.println("This is the complete guide !!!!");
 
 
-        System.out.println("Pakaya Pramuditha");
-        System.out.println("Hello World !!!!");
+//        System.out.println("Pakaya Pramuditha");
+//
+//        System.out.println("Hello World !!!!");
+//
+//        System.out.println("Hello World !!!");
+//
+//        System.out.println("Arkash you are a bright student");
+//
+//        System.out.println("My name is Deelaka Dias.");
+//        System.out.println("Done");
+//
+//        System.out.println("Done the work perfectly");
+//        System.out.println("Pakaya");
+//
+//        System.out.println("Deelaka and Pramuditha Niggas");
 
-        System.out.println("Hello World !!!");
-
-        System.out.println("Arkash you are a bright student");
-
-        System.out.println("My name is Deelaka Dias.");
-        System.out.println("Done");
-
-        System.out.println("Done the work perfectly");
-        System.out.println("Pakaya");
-
-        System.out.println("Deelaka and Pramuditha Niggas");
+        System.out.println("Deelaka is the lord");
+       
 
     }
 }
