174b110d7fd47c1b21781080a78563b2902c9393
Lakshan Cooray <Lakshancooray23@gmail.com>
2023-10-14 08:57:16 +0530

    done

--- ClubManagementSystem/src/main/java/com/example/clubmanagementsystem/HelloApplication.java
+++ ClubManagementSystem/src/main/java/com/example/clubmanagementsystem/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         try{
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ClubAdvisorDashboard.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 1250, 700);
+            Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
             stage.setResizable(false);
             stage.initStyle(StageStyle.UNDECORATED);
             stage.setScene(scene);
