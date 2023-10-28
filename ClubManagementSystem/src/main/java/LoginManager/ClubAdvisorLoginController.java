072ce711f12e2ad2ea97159edfb85a9d1acfac55
Lakshan Cooray <Lakshancooray23@gmail.com>
2023-10-28 17:31:35 +0530

    commited

--- ClubManagementSystem/src/main/java/LoginManager/ClubAdvisorLoginController.java
+++ ClubManagementSystem/src/main/java/LoginManager/ClubAdvisorLoginController.java
@@ -80,5 +80,14 @@ public class ClubAdvisorLoginController {
         stage.show();
     }
 
+    @FXML
+    void GoToRegisterForm(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/RegisterManager/ClubAdvisorRegistration.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 }
