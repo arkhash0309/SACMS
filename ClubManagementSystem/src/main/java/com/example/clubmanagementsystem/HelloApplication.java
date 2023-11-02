8ddc5ec10383f9d3e1f214bce5c56826caf59e58
Pramuditha <pramudithaofficial@gmail.com>
2023-11-03 00:43:08 +0530

    123456

--- ClubManagementSystem/src/main/java/com/example/clubmanagementsystem/HelloApplication.java
+++ ClubManagementSystem/src/main/java/com/example/clubmanagementsystem/HelloApplication.java
@@ -23,7 +23,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("1234565");
         launch();
     }
 }
