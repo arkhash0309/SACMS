d68e607e59de5ab4cd7146a3d41f99a9a06836c1
Lakshan Cooray <Lakshancooray23@gmail.com>
2023-11-28 14:30:47 +0530

    done changes

--- ClubManagementSystem/src/main/java/LoginDashboardManager/LoginController.java
+++ ClubManagementSystem/src/main/java/LoginDashboardManager/LoginController.java
@@ -43,7 +43,6 @@ public class LoginController implements Initializable {
         stage.setX(event.getScreenX() - xPosition);
         stage.setY(event.getScreenY() - yPosition);
     }
-
     @FXML
     void LoginPanePressed(MouseEvent event) {
         xPosition = event.getSceneX();
